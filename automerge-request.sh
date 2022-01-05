4638f215b720a2e9fbe0208bcf22eed98600c731
Johnson, Joanna <jjohnson@iqt.org>
2022-01-05 15:17:57 -0700

    fix automerge request

--- automerge-request.sh
+++ automerge-request.sh
@@ -10,14 +10,14 @@ fi
 
 TITLE="${CI_COMMIT_REF_NAME} to ${TARGET_BRANCH} SYNC"
 
-echo "${CI_SERVER_HOST}/api/v4/projects/${CI_PROJECT_ID}/"
+
 # Get all open merge requests and check if one exists for source branch
-LISTMR=`curl -L --silent "${CI_SERVER_HOST}/api/v4/projects/${CI_PROJECT_ID}/merge_requests?state=opened" --header "PRIVATE-TOKEN:${PERSONAL_ACCESS_TOKEN}"`;
+LISTMR=`curl -L --silent "https://${CI_SERVER_HOST}/api/v4/projects/${CI_PROJECT_ID}/merge_requests?state=opened" --header "PRIVATE-TOKEN:${PROJECT_ACCESS_TOKEN}"`;
 COUNTBRANCHES=`echo ${LISTMR} | grep -o "\"source_branch\":\"${CI_COMMIT_REF_NAME}\"" | wc -l`;
 
 # If no MR found, create a new one
 if [ ${COUNTBRANCHES} -eq "0" ]; then
-  curl -L --request POST "${CI_SERVER_HOST}/api/v4/projects/${CI_PROJECT_ID}/merge_requests?id=${CI_PROJECT_ID}&source_branch=${CI_COMMIT_REF_NAME}&target_branch=${TARGET_BRANCH}&title=${TITLE}&remove_source_branch=true&assignee_id=${GITLAB_USER_ID}" --header "Content-Type:application/json" --header "PRIVATE-TOKEN:${PERSONAL_ACCESS_TOKEN}"
+  curl -L --request POST "https://${CI_SERVER_HOST}/api/v4/projects/${CI_PROJECT_ID}/merge_requests?id=${CI_PROJECT_ID}&source_branch=${CI_COMMIT_REF_NAME}&target_branch=${TARGET_BRANCH}&title=${TITLE}&remove_source_branch=true&assignee_id=${GITLAB_USER_ID}" --header "Content-Type:application/json" --header "PRIVATE-TOKEN:${PROJECT_ACCESS_TOKEN}"
   echo "Opened a new merge request: ${TITLE} and assigned to ${GITLAB_USER_NAME}";
   exit;
 fi
